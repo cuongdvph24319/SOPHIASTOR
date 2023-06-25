4c93bf043edbc4b4bdc518be9a45d7aff6f427d6
cuongdv <cuongdvph24319@fpt.edu.vn>
2023-06-25 23:44:12 +0700

    Initial commit

---
+++ src/main/java/poly/edu/sophiastore/SophiastoreApplication.java
@@ -0,0 +1,13 @@
+package poly.edu.sophiastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SophiastoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SophiastoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/poly/edu/sophiastore/SophiastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package poly.edu.sophiastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SophiastoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
