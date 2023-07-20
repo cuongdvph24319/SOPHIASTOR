b2b7689385a2d4672e911da3bac969b7d34e97e2
cuongdv <cuongdvph24319@fpt.edu.vn>
2023-07-20 17:26:54 +0700

    update voucher

--- src/main/java/poly/edu/sophiastore/entity/Voucher.java
+++ src/main/java/poly/edu/sophiastore/entity/Voucher.java
@@ -2,10 +2,14 @@ package poly.edu.sophiastore.entity;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+import poly.edu.sophiastore.request.VoucherView;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -24,10 +28,18 @@ public class Voucher {
     private UUID id;
     @Column(name = "MaGiamGia")
     private String maGiamGia;
+    @Column(name = "TenGiamGia")
+    private String tenGiamGia;
     @Column(name = "SoLuong")
     private Integer soLuong;
+    @NotNull
+    @FutureOrPresent(message = "Ngày bắt đầu phải là ngày hiện tại hoặc tương lai")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "NgayBatDau")
     private Date ngayBatDau;
+    @NotNull
+    @FutureOrPresent(message = "Ngày bắt đầu phải là ngày hiện tại hoặc tương lai")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "NgayKetThuc")
     private Date ngayKetThuc;
     @Column(name = "TrangThai")
@@ -39,5 +51,16 @@ public class Voucher {
 
     @OneToMany(mappedBy = "id.voucher", fetch = FetchType.LAZY)
     private List<VouCherChiTiet> listVoucher = new ArrayList<>();
+
+    public void loadFormViewModel(VoucherView voucher){
+        this.setMaGiamGia(voucher.getMaGiamGia());
+        this.setTenGiamGia(voucher.getTenGiamGia());
+        this.setSoLuong(voucher.getSoLuong());
+        this.setNgayBatDau(voucher.getNgayBatDau());
+        this.setNgayKetThuc(voucher.getNgayKetThuc());
+        this.setTrangThai(voucher.getTrangThai());
+        this.setDieuKienGiamGia(voucher.getDieuKienGiamGia());
+        this.setPhanTramGiamGia(voucher.getPhanTramGiamGia());
+    }
 
 }
